a6ebb07b6440e0cf9a663a0610395d2d0b0147d7
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2025-07-13 22:39:18 -0500

    Discovery server changes

--- my-github-cleaner-base/src/main/java/me/sathish/my_github_cleaner/base/MyGithubCleanerApplication.java
+++ my-github-cleaner-base/src/main/java/me/sathish/my_github_cleaner/base/MyGithubCleanerApplication.java
@@ -2,9 +2,11 @@ package me.sathish.my_github_cleaner.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan("me.sathish.my_github_cleaner")
 public class MyGithubCleanerApplication {
 
