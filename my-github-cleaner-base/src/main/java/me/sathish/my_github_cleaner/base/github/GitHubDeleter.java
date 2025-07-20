238aba68c011e18076485a78f8247d574060fd50
Sathish Jayapal <sjayapal@skminfotech.com>
2025-07-20 07:27:09 -0500

    Delete repo with JUNIT details

--- my-github-cleaner-base/src/main/java/me/sathish/my_github_cleaner/base/github/GitHubDeleter.java
+++ my-github-cleaner-base/src/main/java/me/sathish/my_github_cleaner/base/github/GitHubDeleter.java
@@ -1,18 +1,28 @@
 package me.sathish.my_github_cleaner.base.github;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.io.IOException;
 
+@Service
 public class GitHubDeleter {
-    public static void deleteRepository(String token, String owner, String repoName) {
+    @Autowired
+    Environment environment;
+
+    public void deleteRepository(String repositoryName) {
+
         try {
             HttpClient client = HttpClient.newHttpClient();
 
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.github.com/repos/" + owner + "/" + repoName))
-                    .header("Authorization", "token " + token)
+                    .uri(URI.create("https://api.github.com/repos/" + environment.getProperty("githubusername") + "/" + repositoryName))
+                    .header("Authorization", "token " + environment.getProperty("GITHUB_TOKEN"))
                     .header("Accept", "application/vnd.github.v3+json")
                     .DELETE()
                     .build();
--- my-github-cleaner-base/src/test/java/me/sathish/my_github_cleaner/base/github/GitHubDeleterServiceTest.java
+++ my-github-cleaner-base/src/test/java/me/sathish/my_github_cleaner/base/github/GitHubDeleterServiceTest.java
@@ -3,17 +3,18 @@ package me.sathish.my_github_cleaner.base.github;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 public class GitHubDeleterServiceTest {
 
     @Autowired
-    private GitHubDeleter gitHubDeleterService;
+    private GitHubDeleter gitHubDeleter;
+    @Autowired
+    Environment environment;
 
     @Test
     void testDeleteRepository() {
-        // You can call the method here but
-        // avoid hitting real GitHub API in integration tests
-        // or mock dependencies if refactored to inject HttpCliendcct
+        gitHubDeleter.deleteRepository("sathishjee");
     }
 }
