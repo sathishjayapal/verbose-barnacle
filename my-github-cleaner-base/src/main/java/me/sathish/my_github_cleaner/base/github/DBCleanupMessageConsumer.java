72f3d01981e6ad19fa860f7b9c36e355ead4ce05
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2025-09-07 21:45:54 -0500

    spotless changes

--- my-github-cleaner-base/src/main/java/me/sathish/my_github_cleaner/base/github/DBCleanupMessageConsumer.java
+++ my-github-cleaner-base/src/main/java/me/sathish/my_github_cleaner/base/github/DBCleanupMessageConsumer.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class DBCleanupMessageConsumer {
     private ObjectMapper objectMapper;
+
     public DBCleanupMessageConsumer(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
-    //TODO 982025 change here.
+    // TODO 982025 change here.
     @RabbitListener(queues = "${garminrun-event.garmin-newrun-queue}")
     public void consumeMessage(String message) {
         System.out.println("Received message: " + message);
