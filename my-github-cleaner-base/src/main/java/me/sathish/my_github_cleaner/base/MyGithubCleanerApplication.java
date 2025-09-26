63e1042ca2f14698fbb312bffba6388c4aa06f2d
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2025-09-25 22:15:37 -0500

    AMQP related changes

--- my-github-cleaner-base/src/main/java/me/sathish/my_github_cleaner/base/MyGithubCleanerApplication.java
+++ my-github-cleaner-base/src/main/java/me/sathish/my_github_cleaner/base/MyGithubCleanerApplication.java
@@ -1,7 +1,9 @@
 package me.sathish.my_github_cleaner.base;
 
+import me.sathish.my_github_cleaner.base.util.RabbitConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
+@EnableConfigurationProperties(RabbitConfigProperties.class)
 @ComponentScan("me.sathish.my_github_cleaner")
 public class MyGithubCleanerApplication {
 
