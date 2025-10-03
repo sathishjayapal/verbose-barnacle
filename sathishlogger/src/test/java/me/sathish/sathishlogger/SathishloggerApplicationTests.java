75558916e8398f1cc43129fed662c0b38dc9d1cf
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2025-10-02 22:12:47 -0500

    For logger aspects

---
+++ sathishlogger/src/test/java/me/sathish/sathishlogger/SathishloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sathish.sathishlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SathishloggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
