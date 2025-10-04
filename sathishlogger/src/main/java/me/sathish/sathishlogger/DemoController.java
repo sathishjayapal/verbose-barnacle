2285226bd5173c125e38e071849ae3479cdac3bf
Sathish Jayapal <sjayapal@skminfotech.com>
2025-10-03 22:00:10 -0500

    Rabbit config items

--- sathishlogger/src/main/java/me/sathish/sathishlogger/DemoController.java
+++ sathishlogger/src/main/java/me/sathish/sathishlogger/DemoController.java
@@ -1,19 +1,22 @@
 package me.sathish.sathishlogger;
 
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+
 public class DemoController {
 
     private final DemoService demoService;
-
+static final Logger log = org.slf4j.LoggerFactory.getLogger(DemoController.class);
     public DemoController(DemoService demoService) {
         this.demoService = demoService;
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "world") String name) {
+        log.error("Hello World");
         return demoService.greet(name);
     }
 
